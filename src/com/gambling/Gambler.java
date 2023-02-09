63133ce67f4be1cb4f0034b2154996400a11c65b
Tanujachaugule <tanujachaugule1999@gmail.com>
2023-02-09 17:19:14 +0530

    Added: Gambler Would Start With Stake And Bet

---
+++ src/com/gambling/Gambler.java
@@ -0,0 +1,13 @@
+package com.gambling;
+
+public class Gambler {
+    final static int stake = 100;
+
+    // every bet is of 1$
+    final static int bet = 1;
+
+    public static void main(String[] args) {
+        System.out.println("stake: " + stake);
+        System.out.println("bet: " + bet);
+    }
+}
